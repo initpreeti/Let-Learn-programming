25b6651a10b000ebba7d9c4704d5e5ea6a0bc0b8
preeti sahu <142586074+initpreeti@users.noreply.github.com>
2023-09-01 13:30:18 +0530

    code completed

--- linearsearch.c
+++ linearsearch.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<conio.h>
-// timecomplexity = O(n) , space complexity = O(1)
+//time complexity= O(n),space complexity=O(1)
 int linear_search(int arr[], int key, int size){
     for(int i=0; i<size; i++){
         if(arr[i] == key){
@@ -14,6 +14,15 @@ int main(int argc, char *argv[]){
     int arr[] = {1,23,12,6,4,32,8};
     int size = sizeof(arr)/sizeof(arr[0]);
     printf("size of array is %d\n", size);
-    // please complete rest of code.
+    int Key =6; // the element yoy want search for
+    int result= linear_search(arr,Key,size);
+    if(result!=-1){
+        printf("Element%d found at index %d\n",Key,result);
+    }
+else{
+    printf("Element %d not found in the array\n",Key);
+
+}
+    // code completed
     return 0;
 }
